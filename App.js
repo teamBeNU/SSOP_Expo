03e88ec8316c4b67bae41e6e9055710e4493bc9b
yyyujinnn <1209yujin@naver.com>
2024-05-14 18:04:53 +0900

    chore: 파이어베이스 연동 해제

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-// import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { initializeApp } from 'expo-firebase-app';
-import 'firebase/firestore';
 
 import Home from './pages/Home';
 import Bluetooth from './pages/Bluetooth';
@@ -16,42 +13,6 @@ import Space from './pages/Space';
 import CreateTeamSp from './pages/CreateTeamSp';
 import CreateCard from './pages/CreateCard';
 import EnterTeamSp from './pages/EnterTeamSp';
-
-// Firebase 초기화
-const firebaseConfig = {
-    "project_info": {
-      "project_number": "159606592589",
-      "project_id": "ssop-2024",
-      "storage_bucket": "ssop-2024.appspot.com"
-    },
-    "client": [
-      {
-        "client_info": {
-          "mobilesdk_app_id": "1:159606592589:android:a75f6df7d604fd523f7804",
-          "android_client_info": {
-            "package_name": "com.teambenu.ssop"
-          }
-        },
-        "oauth_client": [],
-        "api_key": [
-          {
-            "current_key": "AIzaSyDp49SOggdlg_qpt6mREimtX4OJ3DyH4rE"
-          }
-        ],
-        "services": {
-          "appinvite_service": {
-            "other_platform_oauth_client": []
-          }
-        }
-      }
-    ],
-    "configuration_version": "1"  
-};
-
-initializeApp(firebaseConfig);
-
-// Firestore 사용 예시
-const db = firebase.firestore();
 
 // 스택 네비게이터
 const Stack = createStackNavigator();
