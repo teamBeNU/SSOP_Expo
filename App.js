1c823dc517f092283376de4b92b77f5e56b11fa1
yyyujinnn <1209yujin@naver.com>
2024-05-17 16:20:16 +0900

    feat: fonts 적용

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import "react-native-gesture-handler";
+import { useFonts } from 'expo-font';
 
 import Home from './pages/Home';
 import Bluetooth from './pages/Bluetooth';
@@ -11,14 +12,25 @@ import LinkShare from './pages/LinkShare';
 import CheckCard from './pages/CheckCard';
 import MyCard from './pages/MyCard';
 import Space from './pages/Space';
-import CreateTeamSp from './pages/CreateTeamSp';
+import CreateTeamSp from './pages/CreateTeamSp/CreateTeamSp';
 import CreateCard from './pages/CreateCard';
 import EnterTeamSp from './pages/EnterTeamSp';
 
-// 스택 네비게이터
-const Stack = createStackNavigator();
+import Pretendard from './assets/fonts/Pretendard-Regular.otf';
 
 export default function App() {
+  // 폰트 로드
+  const [fontsLoaded] = useFonts({
+    Pretendard : Pretendard
+  });
+
+  if (!fontsLoaded) {
+    return null; // 폰트 로딩이 완료되지 않으면 null을 반환하여 렌더링을 중지
+  }
+
+  // 스택 네비게이터
+  const Stack = createStackNavigator();
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
