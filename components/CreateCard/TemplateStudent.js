861cf0e6c742919b3dc6259ede56c59769d4ff9f
HJ2828 <tjgmlwo0228@gmail.com>
2024-05-29 23:56:32 +0900

    fix: '다음' 버튼 고정

--- components/CreateCard/TemplateStudent.js
+++ components/CreateCard/TemplateStudent.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Dimensions, ScrollView, Image, Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, SafeAreaView, Dimensions, ScrollView, Image, Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback } from "react-native";
 import React, { useState, useEffect, useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -142,6 +142,16 @@ export default function TemplateStudent({navigation}) {
                     color={theme.green}
                     borderWidth={0}
                 />
+            )}
+
+            {step === 0 && (
+                <View style={styles.container}>
+                    <Text> dddd</Text>
+                    <Text> dddd</Text>
+                    <Text> dddd</Text>
+                    <Text> dddd</Text>
+                    <Text> dddd</Text>
+                </View>
             )}
 
             {step === 1 && (
