9a1e88679f83caa3fd2a4e46ebcf7407a7f33f26
HJ2828 <tjgmlwo0228@gmail.com>
2024-05-30 00:00:13 +0900

    feat: 마이페이지 생성

---
+++ pages/MyPage/MyPageStyle.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Dimensions } from "react-native"
+import { theme } from "../../theme"
+
+const { width:SCREEN_WIDTH, height:SCREEN_HEIGHT } = Dimensions.get('window');
+
+export const styles = StyleSheet.create({
+    
+})
