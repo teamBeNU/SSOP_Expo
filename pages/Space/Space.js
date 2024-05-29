9ecd21a94647b00fc56f23145032cb70328dcc97
Sojin Jang <dpreallove@gmail.com>
2024-05-29 22:04:28 +0900

    feat: Space 페이지 생성

--- pages/Space/Space.js
+++ pages/Space/Space.js
@@ -1,4 +1,5 @@
 import { View, Text } from "react-native";
+import { styles } from './SpaceStyle';
 
 function Space() {
     return (
---
+++ pages/Space/SpaceStyle.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { theme } from "../../theme";
+
+export const styles = StyleSheet.create({
+    mainlayout:{      
+        flex: 1,  
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+})
