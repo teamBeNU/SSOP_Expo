70e8057683f3d77d5cee1d9a5546aac7f2cddaf5
yyyujinnn <1209yujin@naver.com>
2024-09-14 02:52:41 +0900

    feat: 블루투스에 리스트형/격자형 토글 적용

--- components/Bluetooth/CardViewsStyle.js
+++ components/Bluetooth/CardViewsStyle.js
@@ -2,8 +2,8 @@ import { StyleSheet } from 'react-native';
 import { theme } from "../../theme";
 
 export const styles = StyleSheet.create({
-    mainlayout:{      
-        flex: 1,  
+    mainlayout: {
+        flex: 1,
         paddingVertical: 8,
         paddingHorizontal: 16,
         backgroundColor: theme.white
@@ -13,8 +13,8 @@ export const styles = StyleSheet.create({
         fontFamily: "PretendardSemiBold",
         fontSize: 20,
         letterSpacing: -1,
-    },    
-    namebox: { 
+    },
+    namebox: {
     },
     range: { //최신순 정렬
         marginTop: 24,
@@ -24,11 +24,11 @@ export const styles = StyleSheet.create({
         fontSize: 14,
         letterSpacing: -1,
     },
-    DownArrowIcon: { 
+    DownArrowIcon: {
         marginTop: 24,
         marginBottom: 12,
     },
-	Text20: {
+    Text20: {
         marginTop: 16,
         marginLeft: 16,
         fontFamily: "PretendardSemiBold",
@@ -63,7 +63,7 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
     },
     line: {
-        borderBottomWidth:1,
+        borderBottomWidth: 1,
         borderBottomColor: theme.gray90,
         marginTop: 8,
         marginBottom: 8,
@@ -96,7 +96,7 @@ export const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         gap: 16,
-     },
+    },
     noCard: {
         color: theme.gray60,
         fontFamily: 'PretendardSemiBold',
@@ -109,10 +109,72 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         gap: 4,
     },
-        newCard: {
-        color:theme.skyblue,
+    newCard: {
+        color: theme.skyblue,
         fontFamily: 'PretendardSemiBold',
         fontSize: 16,
         letterSpacing: -0.2,
+    },
+
+    // 리스트형 카드 스타일
+    ListContainer: {
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        marginBottom: 12,
+        backgroundColor: theme.white,
+        borderColor: theme.gray95,
+        borderRadius: 16,
+        shadowColor: "rgba(73, 81, 100, 0.09)",
+        shadowOffset: {
+            width: 2,
+            height: 2
         },
+        shadowOpacity: 1,
+        shadowRadius: 2,
+        elevation: 5,
+    },
+    gray: {
+        width: 64,
+        height: 64,
+        backgroundColor: theme.gray80,
+        borderRadius: 16,
+    },
+    infoContainer: {
+        marginLeft: 12
+    },
+    rowName: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 6
+    },
+    Text16gray10: {
+        fontFamily: "PretendardSemibold",
+        color: theme.gray10,
+        fontSize: 16,
+        letterSpacing: -0.32
+    },
+    Text16gray50: {
+        fontFamily: "Pretendard",
+        color: theme.gray50,
+        fontSize: 16,
+    },
+    Text14gray30: {
+        marginTop: 8,
+        fontFamily: "Pretendard",
+        color: theme.gray30,
+        fontSize: 14,
+    }, 
+    Text14gray50: {
+        fontFamily: "Pretendard",
+        color: theme.gray50,
+        fontSize: 14,
+        letterSpacing: -0.14,
+    },
+    newCardBtn: {
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 4
+    }
 })
