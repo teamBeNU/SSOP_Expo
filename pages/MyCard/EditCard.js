c87f398d62c7ee976433c0e2297e2f0ba3aa0e8b
mingyeong0210 <mica7150@naver.com>
2024-09-16 18:56:04 +0900

    feat: .나에 대한 기본 정보 수정 UI

--- pages/MyCard/EditCard.js
+++ pages/MyCard/EditCard.js
@@ -1,14 +1,56 @@
-import { Dimensions, View, Text, TouchableOpacity } from "react-native";
+import { Dimensions, View, Text, TouchableOpacity, TextInput } from "react-native";
 import { styles } from "./EditCardStyle";
 import React, { useState } from 'react';
 import CloseIcon from '../../assets/icons/ic_close_regular_line.svg';
+import DoneIcon from '../../assets/icons/ic_done_small_line.svg';
+import DoneIconBlue from '../../assets/icons/ic_done_small_line_blue.svg';
 
 function EditCard() {
+    const [isSecret, setIsSecret] = useState(false);
 
     return (
-        <View>
-            <Text>카드수정이요</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>나에 대한 기본 정보 수정하기</Text>
+
+            <View style={{...styles.inputContainer, marginBottom: 28}}>
+            <Text style={styles.subTitle}>이름*</Text>
+            <TextInput 
+                style={styles.input}
+                placeholder="이름"
+                />
+            </View>
+
+            <View style={{...styles.inputContainer, marginBottom: 28}}>
+            <Text style={styles.subTitle}>한줄소개*</Text>
+            <TextInput 
+                style={styles.input}
+                placeholder="나에 대해 간단히 알려주세요."
+                />
+            </View>
+
+            <View style={{...styles.inputContainer, marginBottom: 28}}>
+            <Text style={styles.subTitle}>MBTI.</Text>
+            <TextInput 
+                style={styles.input}
+                placeholder="MBTI"
+                />
+            </View>
+
+            <View style={styles.line} />
+
+            <View style={styles.inputContainer}>
+            <Text style={styles.subTitle}>생년월일 8자리</Text>
+            <TextInput 
+                style={{...styles.input, marginBottom: 20}}
+                placeholder="YYYY/MM/DD"
+                />
+            <TouchableOpacity style={styles.birthSecret} onPress={() => setIsSecret(!isSecret)}>
+                <DoneIcon style={{width: 16, height:16, color: isSecret ? '#00C2ED' : '#949494'}}/>
+                <Text>생년월일은 나이 계산에만 사용하고 공개 안 할래요</Text>
+            </TouchableOpacity>
+            </View>
         </View>
     );
 }
 export default EditCard;
+
--- pages/MyCard/EditCardStyle.js
+++ pages/MyCard/EditCardStyle.js
@@ -6,7 +6,54 @@ const cardWidth = WIDTH * 0.8;
 const cardHeight = WIDTH * 1.2;
 
 export const styles = StyleSheet.create({
-
-
-    
+    container:{
+    backgroundColor: 'white',
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 32,
+    },
+    title:{
+    color: theme.gray10,
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 20,
+    fontWeight: '600',
+    lineHeight: 24,
+    letterSpacing: -0.4,
+    marginBottom: 48,
+    },
+    inputContainer:{
+    gap: 8,
+    },
+    subTitle:{
+    color: theme.gray30,
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 14,
+    fontWeight: '600',
+    lineHeight: 17,
+    letterSpacing: -0.14,
+    paddingHorizontal: 8
+    },
+    input:{
+    backgroundColor: theme.gray95,
+    height: 48,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    color: theme.gray10,
+    fontFamily: 'PretendardRegular',
+    fontSize: 16,
+    fontWeight: '500',
+    lineHeight: 19,
+    letterSpacing: -0.32
+    },
+    line: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.gray90, 
+    marginBottom: 28,
+    },
+    birthSecret:{
+    flexDirection: 'row',
+    gap: 2,
+    alignItems: 'center',
+    },
 })
