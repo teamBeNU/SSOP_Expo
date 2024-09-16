73f97485591e6606b4e9d4fd21e01a0da4f5cdcc
mingyeong0210 <mica7150@naver.com>
2024-09-16 19:01:34 +0900

    feat: 카드 정보 수정 프로그레스 바

--- pages/MyCard/EditCard.js
+++ pages/MyCard/EditCard.js
@@ -1,55 +1,73 @@
-import { Dimensions, View, Text, TouchableOpacity, TextInput } from "react-native";
+import { Dimensions, View, Text, TouchableOpacity, TextInput, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { styles } from "./EditCardStyle";
 import React, { useState } from 'react';
+import { theme } from "../../theme";
+import * as Progress from 'react-native-progress';
 import CloseIcon from '../../assets/icons/ic_close_regular_line.svg';
 import DoneIcon from '../../assets/icons/ic_done_small_line.svg';
 import DoneIconBlue from '../../assets/icons/ic_done_small_line_blue.svg';
 
 function EditCard() {
     const [isSecret, setIsSecret] = useState(false);
+    const [step, setStep] = useState(1);
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>나에 대한 기본 정보 수정하기</Text>
-
-            <View style={{...styles.inputContainer, marginBottom: 28}}>
-            <Text style={styles.subTitle}>이름*</Text>
-            <TextInput 
-                style={styles.input}
-                placeholder="이름"
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={{flex: 1}}>
+            {( // 프로그레스 바
+                <Progress.Bar
+                    progress={step / 3}
+                    width={null}
+                    height={2}
+                    color={theme.green}
+                    borderWidth={0}
                 />
-            </View>
+            )}
+            {step === 1 && (
+            <View style={styles.container}>
+                <Text style={styles.title}>나에 대한 기본 정보 수정하기</Text>
 
-            <View style={{...styles.inputContainer, marginBottom: 28}}>
-            <Text style={styles.subTitle}>한줄소개*</Text>
-            <TextInput 
-                style={styles.input}
-                placeholder="나에 대해 간단히 알려주세요."
-                />
-            </View>
+                <View style={{...styles.inputContainer, marginBottom: 28}}>
+                <Text style={styles.subTitle}>이름*</Text>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="이름"
+                    />
+                </View>
 
-            <View style={{...styles.inputContainer, marginBottom: 28}}>
-            <Text style={styles.subTitle}>MBTI.</Text>
-            <TextInput 
-                style={styles.input}
-                placeholder="MBTI"
-                />
-            </View>
+                <View style={{...styles.inputContainer, marginBottom: 28}}>
+                <Text style={styles.subTitle}>한줄소개*</Text>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="나에 대해 간단히 알려주세요."
+                    />
+                </View>
 
-            <View style={styles.line} />
+                <View style={{...styles.inputContainer, marginBottom: 28}}>
+                <Text style={styles.subTitle}>MBTI.</Text>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="MBTI"
+                    />
+                </View>
 
-            <View style={styles.inputContainer}>
-            <Text style={styles.subTitle}>생년월일 8자리</Text>
-            <TextInput 
-                style={{...styles.input, marginBottom: 20}}
-                placeholder="YYYY/MM/DD"
-                />
-            <TouchableOpacity style={styles.birthSecret} onPress={() => setIsSecret(!isSecret)}>
-                <DoneIcon style={{width: 16, height:16, color: isSecret ? '#00C2ED' : '#949494'}}/>
-                <Text>생년월일은 나이 계산에만 사용하고 공개 안 할래요</Text>
-            </TouchableOpacity>
+                <View style={styles.line} />
+
+                <View style={styles.inputContainer}>
+                <Text style={styles.subTitle}>생년월일 8자리</Text>
+                <TextInput 
+                    style={{...styles.input, marginBottom: 20}}
+                    placeholder="YYYY/MM/DD"
+                    />
+                <TouchableOpacity style={styles.birthSecret} onPress={() => setIsSecret(!isSecret)}>
+                    <DoneIcon style={{width: 16, height:16, color: isSecret ? '#00C2ED' : '#949494'}}/>
+                    <Text>생년월일은 나이 계산에만 사용하고 공개 안 할래요</Text>
+                </TouchableOpacity>
+                </View>
             </View>
-        </View>
+            )}
+            </View>
+            </TouchableWithoutFeedback>
     );
 }
 export default EditCard;
