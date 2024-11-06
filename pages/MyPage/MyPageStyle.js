92832bfb01f8580b8e0fda515cf7a58a8709de8b
HJ2828 <tjgmlwo0228@gmail.com>
2024-11-06 17:41:37 +0900

    fix: 마이페이지 ui 수정

--- pages/MyPage/MyPageStyle.js
+++ pages/MyPage/MyPageStyle.js
@@ -11,7 +11,7 @@ export const styles = StyleSheet.create({
     },
 
     MyPageMain: {
-        paddingHorizontal: 16,
+        // paddingHorizontal: 16,
         flex: 1,
         backgroundColor: theme.gray99,
         paddingTop: 12,
@@ -105,7 +105,7 @@ export const styles = StyleSheet.create({
     logoutContainer: {
         marginVertical: 20,
         width: '15%',
-        marginHorizontal: 16, 
+        // marginHorizontal: 16, 
     },
     logoutText: {
         fontFamily: "PretendardRegular",
