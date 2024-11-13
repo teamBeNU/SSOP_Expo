55550fcae28c0f22e2f983d3be6bd4c90f301b1d
mingyeong0210 <mica7150@naver.com>
2024-11-14 03:19:52 +0900

    fix: 아바타커버에서도 이미지로 변경

--- components/MyCard/ListCardView.js
+++ components/MyCard/ListCardView.js
@@ -54,7 +54,7 @@ export const ListCardView = ({cardData, deleteMode, selectedCards, setSelectedCa
                   }}>
                 <View style={styles.row2}>
 
-                  {item.card_cover === 'avatar' ? 
+                  {/* {item.card_cover === 'avatar' ? 
                     <View style={[styles.gray, { backgroundColor: getColor(item.avatar.bgColor)}]}>
                     
                     </View>
@@ -66,7 +66,14 @@ export const ListCardView = ({cardData, deleteMode, selectedCards, setSelectedCa
                         style={{ width: 64, height: 64, borderRadius: 16, }}
                       />
                     </View>                 
-                  }
+                  } */}
+                  <View style={[styles.gray]}>
+                      <Image 
+                        source={{ uri: item.profile_image_url }} 
+                        resizeMode="cover"
+                        style={{ width: 64, height: 64, borderRadius: 16, }}
+                      />
+                    </View>       
                   
                   <View style={styles.infoContainer}>
                     <View style={styles.rowName}>
