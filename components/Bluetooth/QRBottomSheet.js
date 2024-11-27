4f271a6484584b5c12fe9104414e17347af69dc6
yyyujinnn <1209yujin@naver.com>
2024-11-27 23:00:48 +0900

    fix: QR 생성/스캔 원하는 곳으로 이동하는 모달

--- components/Bluetooth/QRBottomSheet.js
+++ components/Bluetooth/QRBottomSheet.js
@@ -6,13 +6,13 @@ import "react-native-gesture-handler";
 import { styles } from "./BottomSheetStyle";
 import CloseBtn from '../../assets/icons/close.svg';
 
-export default function QRBottomSheet({modalVisible, setModalVisible, setCreateStep, setStep, setCardTemplate}) {
+export default function QRBottomSheet({ modalVisible, setModalVisible }) {
     const navigation = useNavigation();
     const screenHeight = Dimensions.get("screen").height;
     const panY = useRef(new Animated.Value(screenHeight)).current;    // 애니메이션 초기 상태
     const translateY = panY.interpolate({ // panY에 따라 BottomSheet의 y축 위치를 결정
-    	inputRange: [-1, 0, 1], // inputRage의 -1을 outpuRage의 0으로 치환하기 때문에 panY가 0보다 작아져도 BottomSheet의 y축 위치에는 변화가 없음
-    	outputRange: [0, 0, 1],
+        inputRange: [-1, 0, 1], // inputRage의 -1을 outpuRage의 0으로 치환하기 때문에 panY가 0보다 작아져도 BottomSheet의 y축 위치에는 변화가 없음
+        outputRange: [0, 0, 1],
     });
 
     // 바텀시트를 초기 위치로 올리는 함수
@@ -21,16 +21,16 @@ export default function QRBottomSheet({modalVisible, setModalVisible, setCreateS
         duration: 300,  // 애니메이션 걸리는 시간(ms)
         useNativeDriver: true  // 네이티브 드라이버 사용 여부(필수)
     })
-    
+
     // 바텀시트를 내리는 함수
     const closeBottomSheet = Animated.timing(panY, {
-        toValue: screenHeight,  
+        toValue: screenHeight,
         duration: 300,
-        useNativeDriver: true  
+        useNativeDriver: true
     })
 
     useEffect(() => {
-        if(modalVisible) {
+        if (modalVisible) {
             resetBottomSheet.start();
         }
     })
@@ -53,16 +53,16 @@ export default function QRBottomSheet({modalVisible, setModalVisible, setCreateS
         >
             <View style={styles.container}>
                 <TouchableWithoutFeedback onPress={closeModal}>
-                    <View style={styles.background}/>
+                    <View style={styles.background} />
                 </TouchableWithoutFeedback>
                 <Animated.View
-                    style={{...styles.bottomSheetContainer, transform: [{translateY: translateY}]}}
+                    style={{ ...styles.bottomSheetContainer, transform: [{ translateY: translateY }] }}
                 >
                     <View style={styles.title}>
                         <Text style={styles.titleText}>카드 보내기</Text>
-                        <TouchableOpacity 
+                        <TouchableOpacity
                             style={styles.closeBtn}
-                            onPress={closeModal}    
+                            onPress={closeModal}
                         >
                             <CloseBtn />
                         </TouchableOpacity>
@@ -70,9 +70,8 @@ export default function QRBottomSheet({modalVisible, setModalVisible, setCreateS
                     <View style={styles.btnContainer}>
                         <TouchableOpacity
                             style={styles.btn}
-                            onPress={() => {
-                                navigation.navigate("내 카드 보내기"); 
-                                setStep(1);
+                            onPress={() => {                                
+                                navigation.navigate("내 카드 보내기", { step: 1 });
                             }}
                         >
                             <Text style={styles.btnText}>QR 생성하기</Text>
@@ -81,8 +80,7 @@ export default function QRBottomSheet({modalVisible, setModalVisible, setCreateS
                         <TouchableOpacity
                             style={styles.btn}
                             onPress={() => {
-                                navigation.navigate("내 카드 보내기"); 
-                                setStep(2);
+                                navigation.navigate("내 카드 보내기", { step: 2 });
                             }}
                         >
                             <Text style={styles.btnText}>QR 스캔하기</Text>
