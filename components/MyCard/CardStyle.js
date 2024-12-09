6df0c36df58a57ea29a0c70226177c6e366accd1
mingyeong0210 <mica7150@naver.com>
2024-12-10 00:50:43 +0900

    fix: 카드 앞면 멘트 위치 변경

--- components/MyCard/CardStyle.js
+++ components/MyCard/CardStyle.js
@@ -32,20 +32,21 @@ export const styles = StyleSheet.create({
     },
     cardTextArea: {
         backgroundColor: "white",
-        height: cardHeight * 0.32,
+        height: cardHeight * 0.34,
         padding: 24,
+        paddingBottom: 28,
         borderRadius: 20,
         gap: 12,
         width: '100%',
         position: 'absolute',
         bottom: 0,
-        justifyContent: 'center'
+        //justifyContent: 'center'
     },
     basicInfo: {
         flexDirection: "row",
         alignItems: 'flex-end',
         height: 29,
-        gap: 8
+        gap: 8,
     },
     name: {
         color: theme.gray10,
