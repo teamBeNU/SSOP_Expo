498fdb7c905b79da766bf419ceccacd165bd7784
HJ2828 <tjgmlwo0228@gmail.com>
2024-12-11 22:34:10 +0900

    fix: 에러 나는 코드 제거

--- components/CreateCard/TemplateStudentSchool.js
+++ components/CreateCard/TemplateStudentSchool.js
@@ -393,19 +393,11 @@ export default function TemplateStudentSchool ({navigation, card_template, step,
             navigation.setOptions({
                 headerTitle: '카드 정보 작성하기',
                 headerTitleAlign: 'center',
-                headerTitleStyle: {
-                    ...textStyles.body16m,
-                    color: theme.gray10
-                  },
             });
         } else if (step === 6) {
             navigation.setOptions({
                 headerTitle: '카드 커버 선택하기',
                 headerTitleAlign: 'center',
-                headerTitleStyle: {
-                    ...textStyles.body16m,
-                    color: theme.gray10
-                  },
             });
         } else if (step === 7) {
             navigation.setOptions({
@@ -421,10 +413,6 @@ export default function TemplateStudentSchool ({navigation, card_template, step,
                         <Text style={styles.avatarNext}>완료</Text>
                     </TouchableOpacity>
                 ),
-                headerTitleStyle: {
-                    ...textStyles.body16m,
-                    color: theme.gray10
-                  },
             });
         } else if (step === 8) {
             navigation.setOptions({
@@ -440,10 +428,6 @@ export default function TemplateStudentSchool ({navigation, card_template, step,
                         <HomeIcon style={{marginRight: 20}}/>
                     </TouchableOpacity>
                 ),
-                headerTitleStyle: {
-                    ...textStyles.body16m,
-                    color: theme.gray10
-                  },
             });
         }
     }, [step]);
